926f212f4aa72b3857c089603489f23cd4387f98
shakil1988 <106422958+shakil1988@users.noreply.github.com>
2022-08-12 22:02:06 +0600

    navigation menu format change

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use strict";
+
+  
+    $(window).scroll(function () {
+      var windowhight = $(window).scrollTop();
+  
+      if (windowhight > 10) {
+        $("header").addClass("header-background");
+        $("header").addClass("header-color");
+        $(".nav").addClass("header-color");
+      } else {
+        $("header").removeClass("header-background");
+        $("header").removeClass("header-color");
+        $(".nav").removeClass("header-color");
+      }
+    });
+  })(jQuery);
+  
