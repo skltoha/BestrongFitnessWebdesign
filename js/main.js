3a7682ed24e438bcda9cecf0868c32f595236ad8
shakil1988 <106422958+skltoha@users.noreply.github.com>
2022-08-13 15:28:02 +0600

    main page update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-  
-    $(window).scroll(function () {
-      var windowhight = $(window).scrollTop();
-  
-      if (windowhight > 10) {
-        $("header").addClass("header-background");
-        $("header").addClass("header-color");
-        $(".nav").addClass("header-color");
-      } else {
-        $("header").removeClass("header-background");
-        $("header").removeClass("header-color");
-        $(".nav").removeClass("header-color");
-      }
-    });
+  $(window).scroll(function () {
+    var windowhight = $(window).scrollTop();
 
-  })(jQuery);
+    if (windowhight > 10) {
+      $("header").addClass("header-background");
+      $("header").addClass("header-color");
+      $(".nav").addClass("header-color");
+    } else {
+      $("header").removeClass("header-background");
+      $("header").removeClass("header-color");
+      $(".nav").removeClass("header-color");
+    }
+
+    if (windowhight > 5500) {
+      $("header").hide();
+    } else {
+      $("header").show();
+    }
+  });
+})(jQuery);
